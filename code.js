5f36e3a1701f55c8bf527fb554ceffe4687ee68f
Mohammed Imdad Ulla Khan <68274252+MohammedImdadUllaKhan@users.noreply.github.com>
2020-07-15 14:01:58 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,9 @@
+alert ("hit me!!");
+console.log("hey its my first javascript code");
+console.time("time taken")
+console.log("hey");
+console.error("hey you got an errorxxxx");
+console.warn("!!Warning!!");
+var a ={name:"imdad",age:"23"};
+console.log(a);
+console.timeEnd("time taken")
